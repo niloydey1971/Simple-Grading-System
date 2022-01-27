210e9bce10695626417334888bab699e3ec30667
niloydey1971 <u1304047@student.cuet.ac.bd>
2022-01-28 03:20:23 +0600

    first commit

---
+++ practice1.js
@@ -0,0 +1,22 @@
+var number = 32;
+if (number >= 80 && number <= 100){
+    console.log('A+');
+}
+else if (number >= 70 && number <= 79){
+    console.log('A');
+}
+else if (number >= 60 && number <= 69){
+     console.log('A-');
+}
+else if (number >= 50 && number <= 59){
+     console.log('B');
+}
+else if (number >= 40 && number <= 49){
+     console.log('C');
+}
+else if (number >= 33 && number <= 39){
+     console.log('D');
+}
+else {
+     console.log('tui fail');
+}
